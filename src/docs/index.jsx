7505a81731e0155eb598b3def6e4b230beedb13f
Haleeq Usman <Husman85@gmail.com>
2019-08-03 18:28:19 -0400

    Fix import path for docs

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import Quiz from "../../lib/Quiz";
+import Quiz from "../lib/Quiz";
 import {quiz} from './quiz';
 
 function App() {
